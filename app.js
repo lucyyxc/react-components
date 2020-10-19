304818802f0a9d0ccb8ffa8de8565aa00705c47a
lucyyxc <xilucychen@gmail.com>
2020-10-19 14:42:52 -0700

    completed up to interactive

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// TODO
+
+// var Matcha = () => (
+//   <ul>
+//     <li>matcha</li>
+//   </ul>
+// );
+
+// var Pizza = () => (
+//   <ul>
+//     <li>pizza</li>
+//   </ul>
+// );
+
+
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+  </ul>
+);
+
+var GroceryList = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryListItem groceryItems={['matcha', 'pizza']}/>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'))
